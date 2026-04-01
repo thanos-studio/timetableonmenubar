0197fd5529fc2317c71eb909bf581dad76140952
Taeyeong Kim <taeyeong.kim@algorix.io>
2026-04-01 10:58:03 +0900

    Initial Commit

---
+++ TimeTableOnMenuBar/TimeTableOnMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTableOnMenuBarApp.swift
+//  TimeTableOnMenuBar
+//
+//  Created by Taeyeong Kim on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct TimeTableOnMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
